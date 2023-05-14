8d87a73bf98ad53aa446ee98b3c2b6f00bd6558a
allex <william.al1379@gmail.com>
2023-05-13 23:38:07 -0600

    Add base project and git ignore

---
+++ task.go
@@ -0,0 +1,34 @@
+package store
+
+// Task job to be done or completed
+type Task struct {
+	ID    int    // identifier of the task
+	Title string // Title of the task
+	Done  bool   // If task is completed or not
+}
+
+// Datastore manages a list of tasks stored in memory
+type Datastore struct {
+	tasks []Task
+}
+
+// GetPendingTasks returns all the tasks which need to be done
+func (ds *Datastore) GetTasks() []Task {
+	var pendingTasks []Task
+	for _, task := range ds.tasks {
+		if !task.Done {
+			pendingTasks = append(pendingTasks, task)
+		}
+	}
+	return pendingTasks
+}
+
+func (ds *Datastore) getDoneTasks() []Task {
+	var doneTasks []Task
+	for _, task := range ds.tasks {
+		if task.Done {
+			doneTasks = append(doneTasks, task)
+		}
+	}
+	return doneTasks
+}
---
+++ task_test.go
@@ -0,0 +1,44 @@
+package store
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetPendingTasks(t *testing.T) {
+	t.Log("GetPendingTasks")
+
+	ds := Datastore{
+		tasks: []Task{
+			{1, "Do housework", true},
+			{2, "Buy milk", false},
+		},
+	}
+
+	want := []Task{ds.tasks[1]}
+
+	t.Log("should return the tasks which need to be completed")
+
+	if got := ds.GetTasks(); !reflect.DeepEqual(got, want) {
+		t.Errorf("Got %v wanted %v", got, want)
+	}
+}
+
+func TestGetDoneTasks(t *testing.T) {
+	t.Log("GetPendingTasks")
+
+	ds := Datastore{
+		tasks: []Task{
+			{1, "Do housework", true},
+			{2, "Buy milk", false},
+		},
+	}
+
+	want := []Task{ds.tasks[0]}
+
+	t.Log("should return the tasks which need to be completed")
+
+	if got := ds.getDoneTasks(); !reflect.DeepEqual(got, want) {
+		t.Errorf("Got %v wanted %v", got, want)
+	}
+}
